586c84b76a4015e919fb6f143f84bb4e0de09907
Zidane Sunesara <zidanesunesara786@gmail.com>
2021-10-15 20:05:59 +0530

    Define instance attribute of Point and Rectangle

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 class Rectangle:
-    pass
+    def __init__(self, point1, point2):
+        """
+        point1 and point2 are object of Class Point which will become instance attribute of
+        Rectangle Class this 2 point will create rectangle
+        """
+        self.point1 = point1
+        self.point2 = point2
 
 
 class Point:
-    pass
+    def __init__(self, x, y):
+        """
+        x,y are coordinate which which create Point
+        :param x: coordinate
+        :param y: coordinate
+        """
+        self.x = x
+        self.y = y
