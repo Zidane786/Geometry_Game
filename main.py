3e4a596779764178d894a3ae587a352800c7dee6
Zidane Sunesara <zidanesunesara786@gmail.com>
2021-10-15 20:19:49 +0530

    Define display_coord() instance method which will display lower left and upper right co-ordinate of Rectangle. Define fall_in_rect() instance method which check if point falls inside rectangle or not.If YES than return True .Else return False

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ class Rectangle:
         self.point1 = point1
         self.point2 = point2
 
+    def display_coord(self):
+        """
+        :return:Print Co-ordinate to lower left and upper right of Rectangle
+        """
+        print(f"Rectangle Co-ordinate:- {self.point1.x},{self.point1.y}",
+              f"and {self.point2.x},{self.point2.y}")
+
 
 class Point:
     def __init__(self, x, y):
@@ -17,3 +24,17 @@ class Point:
         """
         self.x = x
         self.y = y
+
+    def fall_in_rect(self, point1, point2):
+        """
+        check if point lies inside rectangle or not!
+
+
+        :param point1: lower left or upper right point of rectangle
+        :param point2: lower left or upper right point of rectangle
+        :return:True if point falls inside rectangle else return False
+        """
+        if point1.x < self.x < point2.x and point1.y < self.y < point2.y:
+            return True
+        else:
+            return False
